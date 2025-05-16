a0fb5a8e9dcfff9107a8078acb73b5908578883e
QiuYang Shen <1741258352@qq.com>
2025-05-16 13:10:38 +0800

    1. 优化 InlineCompletionRenderer.getPreferredWidth 方法，似乎没起作用

--- src/main/kotlin/com/github/xiaohuanxiong3/codecopilot/support/editor/componentManager/renderer/InlineCompletionRenderer.kt
+++ src/main/kotlin/com/github/xiaohuanxiong3/codecopilot/support/editor/componentManager/renderer/InlineCompletionRenderer.kt
@@ -7,10 +7,10 @@ import javax.swing.JScrollPane
  * @Author Handsome Young
  * @Date 2025/5/15 21:38
  */
-class InlineCompletionRenderer(private val component: JComponent, private val myEditorScrollPane: JScrollPane) : MyRenderer(component, myEditorScrollPane) {
+class InlineCompletionRenderer(private val component: JComponent, private val scrollPane: JScrollPane) : MyRenderer(component, scrollPane) {
 
     override fun getPreferredWidth(): Int {
-        return myEditorScrollPane.viewport.width
+        return scrollPane.width - scrollPane.verticalScrollBar.width
     }
 
     // 重写 contains 方法禁止所有鼠标事件？
